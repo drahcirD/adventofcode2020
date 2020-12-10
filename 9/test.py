5304fd79051985199787eab6ce51715e3fd59535
Richard <richard.ericsson@outlook.com>
2020-12-10 08:35:38 +0100

    Fix typo in test

--- 9/test.py
+++ 9/test.py
@@ -7,7 +7,7 @@ def test_1():
     data = [line for line in pathlib.Path("input_test.txt").read_text().split("\n")]
     assert get_result(data, preamble=5) == 127
 
-def test_1():
+def test_2():
     data = [line for line in pathlib.Path("input_test2.txt").read_text().split("\n")]
     assert get_result2(data, find=127) == 62
 
