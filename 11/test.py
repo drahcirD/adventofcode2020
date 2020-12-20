7ed94896fa40e4941273dd55eeee8e5b4face385
Richard <richard.ericsson@outlook.com>
2020-12-20 23:01:52 +0100

    fix typo

--- 11/test.py
+++ 11/test.py
@@ -8,6 +8,6 @@ def test_1():
     assert get_result(data) == 37
 
 
-def test_1():
+def test_2():
     data = [line for line in pathlib.Path("input_test1.txt").read_text().split("\n")]
     assert get_result2(data) == 26
